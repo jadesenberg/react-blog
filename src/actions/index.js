ddeb347803892b111cc7bdf4c59aaed53955ee69
Jade Bautista <edaj.bautista@gmail.com>
2017-05-26 23:09:53 +0800

    Render title list

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,7 +7,7 @@ const API_KEY = '?key=toodamnfilthy';
 
 export function fetchPosts() {
     const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
-
+    
     return {
         type: FETCH_POSTS,
         payload: request
--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,11 +1,37 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
+    componentDidMount(){ //run when Component is load
+        this.props.fetchPosts();
+    }
+
+    renderPosts(){
+        return _.map(this.props.posts, post => { 
+            return (
+                <li className = "list-group-item" key = { post.id }>
+                    { post.title }
+                </li>
+            );
+        });
+    }
+
     render() {
         return (
-            <div> Post Index </div>
+            <div> 
+                <h3>Posts</h3>
+                <ul className = "list-group"> 
+                    { this.renderPosts() } 
+                </ul>
+             </div>
         );
     }
 }
 
-export default PostsIndex;
+function mapStateToProps(state){ // pass state value to post reducer
+    return { posts: state.posts }
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
