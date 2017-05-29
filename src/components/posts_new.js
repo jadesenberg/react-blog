26c46a6870b8587262b10f1757959e5c89940ba9
Jade Bautista <edaj.bautista@gmail.com>
2017-05-29 17:26:03 +0800

    Add form validation

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -4,22 +4,34 @@ import { Field, reduxForm } from 'redux-form';
 class PostsNew extends Component {
 
     renderField(field) {
+        const { meta: { touched, error } } = field;
+        const className = `form-group ${ touched && error ? 'has-danger' : ''}`;
+
         return(
-            <div className = "form-group">
+            <div className = { className }>
                 <label> { field.label } </label>
                 <input 
                     className = "form-control"
                     type="text" 
                     { ...field.input }
                 />
+                <div className = "text-help">
+                    { touched ? error : '' }
+                </div>
             </div>
         );
     }
 
+    onSubmit(values){
+        console.log(value);
+    }
+
     render() {
+        const { handleSubmit } = this.props;
+
         return(
             <div>
-                <form>
+                <form onSubmit = { handleSubmit(this.onSubmit.bind(this)) }>
                     <Field
                         label = "Title"
                         name = "title"
@@ -36,11 +48,32 @@ class PostsNew extends Component {
                         component = { this.renderField }
                     />
                 </form>
+
+                <button type = "submit" className = "btn btn-primary"> Submit </button>
             </div>
         );
     }
 }
 
+function validate(value) {
+    const errors = {};
+
+    if(!value.title) {
+        errors.title = "Enter a title";
+    }
+
+    if(!value.categories) {
+        errors.categories = "Enter some categories";
+    }
+
+    if(!value.content) {
+        errors.content = "Enter some content";
+    }
+
+    return errors;
+}
+
 export default reduxForm({
+    validate, // same as validate: validate
     form: 'PostNewForm'
 })(PostsNew); //just like connect
